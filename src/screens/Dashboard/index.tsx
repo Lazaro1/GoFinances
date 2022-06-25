5fcde7a0930ba54dd0ef9d2299dbb54a66064865
Lazaro1 <lazarolimasilva2@gmail.com>
2022-06-25 20:34:24 -0300

    first commit

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import * as S from "./styles";
+
+export function Dashboard() {
+  return (
+    <S.Container>
+      <S.Title>DashBoard</S.Title>
+    </S.Container>
+  );
+}
