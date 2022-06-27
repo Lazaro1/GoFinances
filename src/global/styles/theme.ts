9d0282b5e291798390f0f32532060655991c53f8
Lazaro1 <lazarolimasilva2@gmail.com>
2022-06-27 19:43:02 -0300

    styled components module

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -7,13 +7,21 @@ export default {
 
     succes: "#12a454",
     succes_light: "rgba(18,164,84,0.5)",
-    
+
     attention: "#e83f5b",
     attention_light: "rgba(232,63,91,05)",
 
     shape: "#fff",
+
     title: "#363f5f",
     text: "#969cb2",
+    text_dark: "#000000",
     background: "#f0f2f5",
+  },
+
+  fonts: {
+    regular: "Poppins_400Regular",
+    medium: "Poppins_500Medium",
+    bold: "Poppins_700Bold",
   },
 };
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,32 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
 import * as S from "./styles";
+import { HighLightCard } from "../../components/HighLightCard";
 
 export function Dashboard() {
   return (
     <S.Container>
-      <S.Title>DashBoard</S.Title>
+      <S.Header>
+        <S.UserWrapper>
+          <S.UserInfo>
+            <S.Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/31623370?v=4",
+              }}
+            />
+            <S.User>
+              <S.UserGreeting>Olá</S.UserGreeting>
+              <S.UserName>Lázaro</S.UserName>
+            </S.User>
+          </S.UserInfo>
+          <S.Icon name="power" />
+        </S.UserWrapper>
+      </S.Header>
+
+      <S.HighLightCards>
+        <HighLightCard />
+        <HighLightCard />
+        <HighLightCard />
+      </S.HighLightCards>
     </S.Container>
   );
 }
