a13351e8ac0d045dc3f79d603ad5f77465c372fb
Lazaro1 <lazarolimasilva2@gmail.com>
2022-10-17 23:40:37 -0300

    change listagem

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
+import { date } from "yup";
+import { categories } from "../../utils/categories";
 import * as S from "./styles";
-
-interface Category {
-  name: string;
-  icon: string;
-}
 
 export interface TransactionCardProps {
   type: "positive" | "negative";
-  title: string;
+  name: string;
   amount: string;
-  category: Category;
+  category: string;
   date: string;
 }
 
@@ -19,9 +16,10 @@ interface Props {
 }
 
 export function TransactionCard({ data }: Props) {
+  const [category] = categories.filter((item) => item.key === data.category);
   return (
     <S.Container>
-      <S.Title>{data.title}</S.Title>
+      <S.Title>{data.name}</S.Title>
       <S.Amount type={data.type}>
         {data.type === "negative" && "- "}
         {data.amount}
@@ -29,8 +27,8 @@ export function TransactionCard({ data }: Props) {
 
       <S.Footer>
         <S.Category>
-          <S.Icon name={data.category.icon} />
-          <S.CategoryName>{data.category.name}</S.CategoryName>
+          <S.Icon name={category.icon} />
+          <S.CategoryName>{category.name}</S.CategoryName>
         </S.Category>
         <S.Date>{data.date}</S.Date>
       </S.Footer>
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,41 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./styles";
 import { HighLightCard } from "../../components/HighLightCard";
 import {
   TransactionCard,
   TransactionCardProps,
 } from "../../components/TransactionCard";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
 
 export interface DatalistProps extends TransactionCardProps {
   id: string;
 }
 export function Dashboard() {
-  const data: DatalistProps[] = [
-    {
-      id: "1",
-      type: "positive",
-      title: "Desenvolvimento de Site",
-      amount: "R$ 12,00,00",
-      category: { name: "Vendas", icon: "dollar-sign" },
-      date: "17/06/2020",
-    },
-    {
-      id: "2",
-      type: "negative",
-      title: "Desenvolvimento de Site",
-      amount: "R$ 12,00,00",
-      category: { name: "Alimentação", icon: "coffee" },
-      date: "17/06/2020",
-    },
-    {
-      id: "3",
-      type: "negative",
-      title: "Aluguel do Apartamento",
-      amount: "R$ 12,00,00",
-      category: { name: "Casa", icon: "shopping-bag" },
-      date: "17/06/2020",
-    },
-  ];
+  const [data, setData] = useState<DatalistProps[]>([]);
+  const dataKey = "@gofinance:transactions";
+
+  async function loadTransactions() {
+    const response = await AsyncStorage.getItem(dataKey);
+    const transactions = response ? JSON.parse(response) : [];
+
+    const transactionsFormated: DatalistProps[] = transactions.map(
+      (item: DatalistProps) => {
+        const amount = Number(item.amount).toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        });
+
+        const data = new Date(item?.date);
+        const daterFormated = Intl.DateTimeFormat("pt-BR", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "2-digit",
+        }).format(data);
+
+        return {
+          id: item.id,
+          name: item.name,
+          amount,
+          type: item.type,
+          category: item.category,
+          date: daterFormated,
+        };
+      }
+    );
+
+    setData(transactionsFormated);
+  }
+
+  useFocusEffect(() => {
+    loadTransactions();
+  }, []);
+
+  useEffect(() => {
+    async function clean() {
+      await AsyncStorage.removeItem(dataKey);
+    }
+
+    async function getITem() {
+      const response = await AsyncStorage.getItem(dataKey);
+
+      console.log(response, "_______responde");
+    }
+
+    // clean();
+
+    // getITem();
+  }, []);
+
   return (
     <S.Container>
       <S.Header>
